3aab0430a21930e72fe69f1f872ea599967d9f5e
KerenBermeo <bermeokeren@gmail.com>
2024-02-25 00:05:00 -0500

    final update

--- backend/extractToSendData/collectMailPaths.go
+++ backend/extractToSendData/collectMailPaths.go
@@ -2,70 +2,32 @@ package data
 
 import (
 	"fmt"
-	"io"
+	"io/fs"
 	"log"
-	"os"
 	"path/filepath"
-	"sync"
 )
 
 // Devuelve una lista de paths de archivos encontrados.
 func CollectMailsPaths(rootPath string) ([]string, error) {
-	var mailsPathsMu sync.Mutex
-	mailsPaths := make(map[string]bool)
+	var slicePaths []string
 
-	var wg sync.WaitGroup
-	semaphore := make(chan struct{}, 32)
-	filesCh := make(chan string)
-	log.Println("Inicio recoleccion de paths -->")
-
-	// Función que representa a los trabajadores
-	worker := func() {
-		defer wg.Done()
-		for path := range filesCh {
-			mailsPathsMu.Lock()
-			mailsPaths[path] = true
-			mailsPathsMu.Unlock()
-			<-semaphore
-		}
-	}
-
-	// Iniciar trabajadores
-	for i := 0; i < 32; i++ {
-		wg.Add(1)
-		go worker()
-	}
-
-	// Función para caminar por los archivos y enviarlos al canal filesCh
-	walkFunc := func(path string, fileInfo os.FileInfo, err error) error {
-
+	err := filepath.WalkDir(rootPath, func(path string, entry fs.DirEntry, err error) error {
 		if err != nil {
-			return err
+			fmt.Printf("Error al acceder a %s: %v\n", path, err)
+			return err // Devuelve el error encontrado
 		}
-		if !fileInfo.IsDir() {
-			semaphore <- struct{}{}
-			filesCh <- path
+		if !entry.IsDir() {
+			slicePaths = append(slicePaths, path)
 		}
-		return nil
-	}
 
-	err := filepath.Walk(rootPath, walkFunc)
-	if err != nil && err != io.EOF {
-		return nil, fmt.Errorf("error al recopilar paths: %s", err)
-	}
+		return nil // Continuar caminando
+	})
 
-	close(filesCh)
-	wg.Wait()
-
-	// Convertir el mapa a un slice de strings
-	var result []string
-	mailsPathsMu.Lock()
-	for path := range mailsPaths {
-		result = append(result, path)
+	if err != nil {
+		return nil, fmt.Errorf("error al caminar por el directorio: %v", err)
 	}
-	mailsPathsMu.Unlock()
 
 	log.Println("Todos los paths han sido recopilados --> --> ")
 
-	return result, nil
+	return slicePaths, nil
 }
